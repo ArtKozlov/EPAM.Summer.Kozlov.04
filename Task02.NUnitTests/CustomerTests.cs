fe256306d78b9ec5b1a8a4971ed32971a3b57f2c
Artsiom <Artsiomxxx@gmail.com>
2016-07-08 01:39:53 +0300

    did the second task.

---
+++ Task02.NUnitTests/CustomerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task02.NUnitTests
+{
+    public class CustomerTests
+    {
+        [TestCase("artyom", 1000, "+375(44)1111111","N", Result = "Customer record: artyom")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "Ph", Result = "Customer record: +375(44)1111111")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "R", Result = "Customer record: 1000")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "NPh", Result = "Customer record: artyom, +375(44)1111111")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "PhR", Result = "Customer record: +375(44)1111111, 1000")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "NR", Result = "Customer record: artyom, 1000")]
+        [TestCase("artyom", 1000, "+375(44)1111111", "NPhR", Result = "Customer record: artyom, +375(44)1111111, 1000")]
+        public string PositiveTestGetCustomerInfo(string name, decimal revanue, string contactPhone, string format)
+        {
+            Customer client = new Customer(name, revanue, contactPhone);
+            return client.ToString(format);
+        }
+
+        [TestCase("artyom", 1000, "+375(44)1111111", "Re")]
+        public void InputBadFormatForGetCustomerInfo(string name, decimal revanue, string contactPhone, string format)
+        {
+            Customer client = new Customer(name, revanue, contactPhone);
+            Assert.Throws<FormatException>(() => client.ToString(format));
+        }
+        [TestCase("artyom", 1000, "+375(44)1111111", null)]
+        public void InputArgumentNullForGetCustomerInfo(string name, decimal revanue, string contactPhone, string format)
+        {
+            Customer client = new Customer(name, revanue, contactPhone);
+            Assert.Throws<ArgumentNullException>(() => client.ToString(format));
+        }
+    }
+}
---
+++ Task02/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace Task02
+{
+    public class Customer
+    {
+        private string name;
+        private decimal revenue;
+        private string contactPhone; 
+        public Customer(string name, decimal revenue, string contactPhone)
+        {
+            this.name = name;
+            this.revenue = revenue;
+            this.contactPhone = contactPhone;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public decimal Revenue
+        {
+            get { return revenue; }
+        }
+
+        public string ContactPhone
+        {
+            get { return contactPhone; }
+        }
+
+        public string ToString(string format)
+        {
+            if (format == null)
+            {
+                throw new ArgumentNullException();
+            }
+            string answer = "Customer record: ";
+            switch (format)
+            {
+                case "Ph":
+                    return answer + ContactPhone;
+                case "R":
+                    return answer + Revenue.ToString();
+                case "N":
+                    return answer + Name;
+                case "NPhR":
+                    return answer + Name + ", " + ContactPhone + ", " + Revenue.ToString();
+                case "NPh":
+                    return answer + Name + ", " + ContactPhone;
+                case "NR":
+                    return answer + Name + ", " + Revenue.ToString();
+                case "PhR":
+                    return answer + ContactPhone + ", " + Revenue.ToString();
+                default:
+                    throw new FormatException(String.Format("The '{0}' format string is not supported.", format));
+            }
+        }
+
+    }
+}
