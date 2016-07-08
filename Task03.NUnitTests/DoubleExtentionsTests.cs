e5bfb99b0158e3ec980ea292ef0da5188fdac39a
Artsiom <Artsiomxxx@gmail.com>
2016-07-08 05:07:10 +0300

    did task03.

---
+++ Task03.NUnitTests/DoubleExtentionsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task03.NUnitTests
+{
+    public class DoubleExtentionsTests
+    {
+        [TestCase(34.8, Result = "100000001000001011001100110011001100110011001100110011001100110")]
+        [TestCase(100.95, Result = "100000001011001001111001100110011001100110011001100110011001101")]
+        public string ConvertDoubleToBinaryTest(double number)
+        {
+            return number.GetBinaryRepresentationOfNumber();
+        }
+    }
+}
---
+++ Task03/DoubleExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task03
+{
+    public static class DoubleExtentions
+    {
+        /// <summary>
+        /// The method converts the double value to binary value.
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <returns>string binary number</returns>
+        public static string GetBinaryRepresentationOfNumber(this Double number)
+        {
+            long currentNumber = BitConverter.DoubleToInt64Bits(number);
+            string result = Convert.ToString(Convert.ToInt64(currentNumber.ToString()), 2);
+            // can do in any number system, changing the argument 2 to 8,10,16.
+            return result;
+            // I can try to do a return value is long, but it's so large.
+        }
+    }
+}
